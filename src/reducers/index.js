5c83832b3cc8fc54264cba8a9dd153b349bfa672
azharie_muhammad <you@example.com>
2019-07-08 18:23:58 +0700

    remove unused code

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import postReducer from './postsReducer'
-import commentReducer from "./commentsReducer";
-
-export const rootReducer = combineReducers({
-    postReducer,
-    commentReducer
-});
